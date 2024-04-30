3716f3f9bf95ea4cb9e56d51cbdce83d94b49373
Efraim Nabil <efraimefefraim@gmail.com>
2024-04-30 18:46:02 +0300

    Refactor Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,13 +9,11 @@ const Navbar = () => {
         dispatch(setName(''));
     }
     return ( 
-        <div className="navbar bg-violet-600 text-neutral-content"> 
-            <div className="containerWrap flex justify-between">
-            <a className=" text-xl text-white">Chat app</a>
-            <button className="btn btn-ghost text-xl" onClick={logout}>
+        <div className="bg-violet-600 text-neutral-content flex justify-between items-center p-4 h-10">
+            <p className="text-lgtext-white">Chat app</p>
+            <button className="btn btn-ghost text-lg" onClick={logout}>
                 Logout
             </button>
-            </div>
         </div>
        
  
