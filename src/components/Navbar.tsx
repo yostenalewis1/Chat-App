85171cfc62893771b54111fb977bb9d392430f9d
Efraim Nabil <efraimefefraim@gmail.com>
2024-04-30 18:40:44 +0300

    Refactor Navbar component to use Redux for user name and add logout functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,21 @@
+import { useDispatch } from "react-redux";
+import { setName } from "../app/features/userSlice";
+
  
 
 const Navbar = () => {
+    const dispatch = useDispatch();
+    const logout = () => {
+        dispatch(setName(''));
+    }
     return ( 
         <div className="navbar bg-violet-600 text-neutral-content"> 
-             <div className="containerWrap flex justify-between">
-           <a className=" text-xl text-white">Chat app</a>
-           <button className="btn btn-ghost text-xl">Logout</button>
-           </div>
+            <div className="containerWrap flex justify-between">
+            <a className=" text-xl text-white">Chat app</a>
+            <button className="btn btn-ghost text-xl" onClick={logout}>
+                Logout
+            </button>
+            </div>
         </div>
        
  
